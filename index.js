c5596eb49f9a576396a0574e3ca3dc6eb6b8bb34
Balt <heptor42@gmail.com>
2023-03-17 23:43:36 -0500

    wayyy better zoom handling

--- index.js
+++ index.js
@@ -90,16 +90,109 @@ function loadFont(buf) {
     return font;
 }
 
+let offset = {"x": 0, "y": 0};
+let zoom = 1.0;
+
+// For if the user is using a trackpad
+
+const mouseWheel = {"x": 0, "y": 0};
+
+let wheelTimeout;
+let initialZoom = zoom;
+window.addEventListener("wheel", (event) => {
+    clearTimeout(wheelTimeout);
+    if (event.ctrlKey) {
+        initialZoom -= event.deltaY / 10;
+        zoom = Math.min(16, Math.max(1, Math.floor(initialZoom)));
+    } else {
+        mouseWheel.x = -event.deltaX / zoom;
+        mouseWheel.y = -event.deltaY / zoom;
+    }
+    wheelTimeout = setTimeout(()=>{
+        mouseWheel.x = 0; mouseWheel.y = 0; initialZoom = zoom;
+    }, 20);
+}, false);
+
+// For if the user has a touchscreen
+
+let initOffset;
+let initZoom;
+const initTouch = {"bbox": null, "meanPos": null};
+const touch = {"bbox": null, "meanPos": null};
+
+
+window.addEventListener("touchstart", function (e) {
+    e.preventDefault();
+    initOffset = {"x": -offset.x, "y": -offset.y};
+    initZoom = zoom;
+    initTouch.bbox = {"x": Infinity, "y": Infinity, "u": -Infinity, "v": -Infinity};
+    for (let i = 0; i < e.touches.length; i++) {
+        let t = e.touches[i];
+        initTouch.bbox.x = Math.min(initTouch.bbox.x, t.clientX);
+        initTouch.bbox.y = Math.min(initTouch.bbox.y, t.clientY);
+        initTouch.bbox.u = Math.max(initTouch.bbox.u, t.clientX);
+        initTouch.bbox.v = Math.max(initTouch.bbox.v, t.clientY);
+    }
+    initTouch.meanPos = {
+        "x": (initTouch.bbox.x + initTouch.bbox.u) / 2,
+        "y": (initTouch.bbox.y + initTouch.bbox.v) / 2
+    };
+});
+
+window.addEventListener("touchmove", function (e) {
+    e.preventDefault();
+    touch.bbox = {"x": Infinity, "y": Infinity, "u": -Infinity, "v": -Infinity};
+    for (let t of Array.from(e.touches)) {
+        touch.bbox.x = Math.min(touch.bbox.x, t.clientX);
+        touch.bbox.y = Math.min(touch.bbox.y, t.clientY);
+        touch.bbox.u = Math.max(touch.bbox.u, t.clientX);
+        touch.bbox.v = Math.max(touch.bbox.v, t.clientY);
+    }
+    touch.meanPos = {
+        "x": (touch.bbox.x + touch.bbox.u) / 2,
+        "y": (touch.bbox.y + touch.bbox.v) / 2
+    };
+    offset = {
+        "x": -Math.floor(initOffset.x + (touch.meanPos.x - initTouch.meanPos.x) / zoom), 
+        "y": -Math.floor(initOffset.y + (touch.meanPos.y - initTouch.meanPos.y) / zoom)
+    }
+    if (e.touches.length > 1) {
+        let rawZoom = Math.max(
+            Math.abs(touch.bbox.u - touch.bbox.x) / Math.abs(initTouch.bbox.u - initTouch.bbox.x),
+            Math.abs(touch.bbox.v - touch.bbox.y) / Math.abs(initTouch.bbox.v - initTouch.bbox.y)
+        );
+        zoom = Math.floor(initZoom * rawZoom);
+        zoom = Math.min(Math.max(zoom, 1), 1 << 4);
+    }
+});
+
 const canvas = document.getElementById("output");
 
 // Expose a few variables for debugging and/or anyone who wants to mess around in the devtools
 
-let zoom;
 let imageSize;
-let offset;
 let pan;
 let font;
 let done;
+let io;
+let oldKeysDown = new Array(512);
+oldKeysDown.fill(false);
+Object.seal(oldKeysDown);
+let keysJustDown = new Array(512);
+keysJustDown.fill(false);
+Object.seal(keysJustDown);
+let popups;
+let menuOffset;
+let screenSize;
+let mousePos;
+let lastFrameDeltas = new Array(300);
+lastFrameDeltas.fill(17);
+let trackpadMode = window.localStorage.getItem("trackpadMode");
+if (trackpadMode == null) {
+    trackpadMode = false;
+} else {
+    trackpadMode = trackpadMode === "true";
+};
 
 (async function() {
     await ImGui.default();
@@ -110,17 +203,13 @@ let done;
     // Loop variables
     let files = null;
     let texture = gl.createTexture();
+    texture.image = new Image();
     imageSize = {
         "w": null,
         "h": null
     }
-    offset = {
-        "x": 0,
-        "y": 0
-    };
-    zoom = 1.0;
     pan = {"origin": null, "offset": {"x": 0, "y": 0}}
-    let io = ImGui.GetIO();
+    io = ImGui.GetIO();
     font = null;
 
     function loadFiles(files) {
@@ -161,29 +250,52 @@ let done;
             }
         });
     }
+
+    let frameTime = performance.now();
+    
     window.requestAnimationFrame(_loop);
+    const frameDelta = Math.floor(1000 / 60); // Max out at 60 fps for high refresh rate displays
 
     function _loop(time) {
+        lastFrameDeltas.push(performance.now() - frameTime);
+        lastFrameDeltas = lastFrameDeltas.splice(1);
+        frameTime = performance.now();
         ImGui_Impl.NewFrame(time);
         ImGui.NewFrame();
-        let popups = [];
-        let menuOffset = 0;
-        let screenSize = {w: io.DisplaySize.x, h: io.DisplaySize.y};
-        let mousePos = ImGui.GetMousePos();
+        for (let i = 0; i < io.KeysDown.length; i++) {
+            keysJustDown[i] = io.KeysDown[i] & !oldKeysDown[i];
+        }
+        popups = [];
+        menuOffset = 0;
+        screenSize = {w: io.DisplaySize.x, h: io.DisplaySize.y};
+        mousePos = ImGui.GetMousePos();
 
         // --- Drawing the UI ---
         if (ImGui.BeginMainMenuBar()) {
             menuOffset = ImGui.GetWindowSize().y;
+            screenSize.y -= menuOffset;
             if (ImGui.BeginMenu("File", true)) {
                 if (ImGui.MenuItem("Open", null, false, true)) {
                     openFiles().then(loadFiles);
                 }
                 ImGui.EndMenu();
             }
+            if (ImGui.BeginMenu("Controls", true)) {
+                ImGui.Text(`- Right click to pan while using mouse`);
+                ImGui.Text(`- Pinch to zoom, one finger to pan while using touchscreen`);
+                ImGui.Text(`- Pinch to zoom, two fingers to pan while using trackpad`);
+                ImGui.Text(`  - If using trackpad, enable`);
+                ImGui.SameLine();
+                ImGui.PushStyleVar(ImGui.StyleVar.FramePadding, new ImGui.Vec2(0, 0));
+                changedMode = ImGui.Checkbox("##trackpad", (_ = trackpadMode) => trackpadMode = _);
+                if (changedMode) window.localStorage.setItem("trackpadMode", trackpadMode);
+                ImGui.PopStyleVar();
+                ImGui.EndMenu();
+            }
             ImGui.EndMainMenuBar();
         }
         ImGui.SetNextWindowSize(
-            new ImGui.Vec2(screenSize.w, screenSize.h - menuOffset)
+            new ImGui.Vec2(screenSize.w, screenSize.h)
         );
         ImGui.SetNextWindowPos(
             new ImGui.Vec2(0, menuOffset)
@@ -208,52 +320,96 @@ let done;
             let mouseY = mousePos.y;
             mouseY -= menuOffset;
             let drawList = ImGui.GetWindowDrawList();
-            if (imageSize.w != null & imageSize.h != null) {
-                drawList.AddImage(
-                    texture,
-                    new ImGui.Vec2(Math.floor(offset.x), Math.floor(offset.y + menuOffset)),
-                    new ImGui.Vec2(Math.floor(offset.x + imageSize.w * zoom), Math.floor(offset.y + imageSize.h * zoom + menuOffset))
-                );
-            }
+            let didZoom = Math.abs(io.MouseWheel) > 0 & (!trackpadMode);
             if (ImGui.IsWindowHovered()) {
-                if (Math.abs(io.MouseWheel) > 0) {
-                    let targetPoint = {"x": (mouseX - offset.x) / zoom, "y": (mouseY - offset.y) / zoom}
-                    let strength = Math.sign(io.MouseWheel);
-                    if (zoom <= 1) {
-                        strength = Math.max(0, strength);
-                    } else if (zoom >= 1 << 4) {
-                        strength = Math.min(0, strength);
-                    }
-                    zoom = 2 ** (Math.log2(zoom) + strength);
-                    offset.x = -targetPoint.x * zoom + mouseX
-                    offset.y = -targetPoint.y * zoom + mouseY
-                    if (pan.origin != null) { // If currently panning
-                        pan.offset = {"x": offset.x, "y": offset.y}; // Update offset
-                    }
-                }
                 if (io.MouseDown[1]) {
-                    if (pan.origin == null) { // Just started panning
+                    if (pan.origin == null) {
                         pan.origin = {"x": mouseX, "y": mouseY};
                         pan.offset = {"x": offset.x, "y": offset.y}; // Clone offset
                     }
-                    offset.x = mouseX - pan.origin.x + pan.offset.x;
-                    offset.y = mouseY - pan.origin.y + pan.offset.y;
-                } else if (pan.origin != null) { // Stopped panning
-                    pan.origin = null;
-                    pan.offset = {"x": 0, "y": 0};
+                } else {
+                    if (pan.origin != null) {
+                        pan.origin = null;
+                        pan.offset = {"x": 0, "y": 0};
+                    }
                 }
+                let strength = 2 ** Math.sign(io.MouseWheel);
+                didZoom &= 1 <= zoom * strength & zoom * strength <= 16;
                 zoom = Math.min(Math.max(zoom, 1), 1 << 4);
+                if (didZoom) {
+                    let oldZoom = zoom;
+                    zoom *= strength;
+                    if (1 <= zoom & zoom <= 16) {
+                        offset = {
+                            "x": (offset.x + ((mouseX - screenSize.w / 2) / Math.max(zoom, oldZoom)) * Math.sign(zoom - oldZoom)), 
+                            "y": (offset.y + ((mouseY - screenSize.h / 2) / Math.max(zoom, oldZoom)) * Math.sign(zoom - oldZoom))
+                        };
+                        pan.offset = {"x": offset.x, "y": offset.y};
+                    }
+                    zoom = Math.min(Math.max(zoom, 1), 1 << 4);
+                } else if (trackpadMode) {
+                    offset.x -= mouseWheel.x;
+                    offset.y -= mouseWheel.y;
+                }
+                if (pan.origin != null) {
+                    if (didZoom) {
+                        pan.origin = {"x": mouseX, "y": mouseY};
+                        pan.offset = {"x": offset.x, "y": offset.y};
+                    } else {
+                        offset.x = (pan.origin.x - mouseX) / zoom + pan.offset.x;
+                        offset.y = (pan.origin.y - mouseY) / zoom + pan.offset.y;
+                    }
+                }
             }
+            let fixedOffset = {
+                "x": Math.floor(-offset.x * zoom + (screenSize.w / 2)),
+                "y": Math.floor(-offset.y * zoom + (screenSize.h / 2)) + menuOffset
+            };
+            if (imageSize.w != null & imageSize.h != null) {
+                drawList.AddImage(
+                    texture,
+                    new ImGui.Vec2(Math.floor(fixedOffset.x), Math.floor(fixedOffset.y)),
+                    new ImGui.Vec2(Math.floor(fixedOffset.x + imageSize.w * zoom), Math.floor(fixedOffset.y + imageSize.h * zoom))
+                );
+            }
+            let lines = Array.of(
+                `${Math.floor(1000 / (lastFrameDeltas.reduce((a, b) => a + b) / lastFrameDeltas.length))} FPS`,
+                `${Math.floor(offset.x)} ${Math.floor(offset.y)} ${zoom}x`
+            );
+            let textWidth = 0;
+            let textHeight = 0;
+            for (let line of lines) {
+                textWidth = Math.max(textWidth, 7 * line.length + 1);
+                textHeight += ImGui.GetTextLineHeight();
+            }
+            drawList.AddRect(
+                new ImGui.Vec2(0, menuOffset),
+                new ImGui.Vec2(textWidth, menuOffset + textHeight),
+                ImGui.GetColorU32(ImGui.Col.Border),
+                0.0,
+                0,
+                3.0
+            );
+            drawList.AddRectFilled(
+                new ImGui.Vec2(0, menuOffset),
+                new ImGui.Vec2(textWidth, menuOffset + textHeight),
+                ImGui.GetColorU32(ImGui.Col.PopupBg)
+            );
+            ImGui.PushStyleVar(ImGui.StyleVar.ItemSpacing, new ImGui.Vec2(0, 0));
+            for (let line of lines) {
+                ImGui.TextDisabled(line);
+            }
+            ImGui.PopStyleVar();
             if (font != null) {
                 // The length HAS to be 256. There's no way without tampering to have it not be here.
                 for (let i = 0; i < 256; i++) {
                     let glyph = font.glyphs[i];
                     if (glyph != null) {
                         let glyphBBox = {};
-                        glyphBBox.x = Math.floor(glyph.srcBBox.x * zoom + offset.x);
-                        glyphBBox.y = Math.floor(glyph.srcBBox.y * zoom + offset.y + menuOffset);
-                        glyphBBox.u = Math.floor(glyph.srcBBox.u * zoom + offset.x);
-                        glyphBBox.v = Math.floor(glyph.srcBBox.v * zoom + offset.y + menuOffset);
+                        glyphBBox.x = Math.floor(glyph.srcBBox.x * zoom + fixedOffset.x);
+                        glyphBBox.y = Math.floor(glyph.srcBBox.y * zoom + fixedOffset.y);
+                        glyphBBox.u = Math.floor(glyph.srcBBox.u * zoom + fixedOffset.x);
+                        glyphBBox.v = Math.floor(glyph.srcBBox.v * zoom + fixedOffset.y);
                         let borderColor = ImGui.GetColorU32(new ImGui.Vec4(1., 1., 1., Math.min(0.5, zoom / 8)));
                         let fillColor   = ImGui.GetColorU32(new ImGui.Vec4(1., 1., 1., Math.min(0.125, zoom / 32)));
                         if (
@@ -310,7 +466,18 @@ let done;
         
         gl.clear(gl.COLOR_BUFFER_BIT);
         ImGui_Impl.RenderDrawData(ImGui.GetDrawData());
-        window.requestAnimationFrame(done ? _done : _loop);
+        
+        for (let i = 0; i < oldKeysDown.length; i++) {
+            oldKeysDown[i] = io.KeysDown[i];
+        }
+        
+        if (performance.now() - frameTime >= frameDelta) {
+            window.requestAnimationFrame(done ? _done : _loop);
+        } else {
+            setTimeout(()=>{
+                window.requestAnimationFrame(done ? _done : _loop);
+            }, Math.max(frameDelta - lastFrameDeltas[0], 0));
+        }
     }
 
     function _done() {
